d2f12cba4ec4c12ac1c2d9dc38df89ceaeb0ddd2
ItsJonQ <itsjonq@gmail.com>
2014-09-06 19:53:30 -0400

    Setu yeoman webapp and yeoman backbone

---
+++ test/app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Sweather, $*/
+
+
+window.Sweather = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Sweather.init();
+});
