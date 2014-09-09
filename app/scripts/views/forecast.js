45f9acd098f0dfd061722860d76a0873a88712ba
ItsJonQ <itsjonq@gmail.com>
2014-09-08 20:44:26 -0400

    Updated forecast view. Added comments

--- app/scripts/views/forecast.js
+++ app/scripts/views/forecast.js
@@ -8,29 +8,28 @@ var Forecast = (function() { 'use strict';
     template: _.template($('#template-forecast').html()),
 
     initialize: function(options) {
+      // Assigning the application.view to this view
       this.application = options.application;
-      this.model.on('change', this.render, this);
-
-      this.$title = this.$el.find('#apparel-title');
+      // Render when model.apparel is set
+      this.model.on('change:apparel', this.render, this);
     },
 
     render: function() {
       var self = this;
+      // Remove the loading state
       self.application.renderLoadComplete();
+      // Hide the element for animation
       self.$el.hide();
-
-      self.model.toCelsius();
-
+      // Render the template into the DOM
       self.$el.html(self.template(self.model.attributes));
-
+      // Fade the template into view
       self.$el.fadeIn('slow', function() {
-
+        // Adjust the background colour after animating the apparel
         if(self.model.get('sweater')) {
           self.application.renderCoolish();
         } else {
           self.application.renderWarmish();
         }
-
       });
 
       return this;
