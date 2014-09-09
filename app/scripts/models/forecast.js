d6cbc17218bc62161bc77b80836650fc826e862a
ItsJonQ <itsjonq@gmail.com>
2014-09-08 21:19:48 -0400

    Removed location model from forecast model. Updated fetching method.

--- app/scripts/models/forecast.js
+++ app/scripts/models/forecast.js
@@ -1,8 +1,6 @@
 /*global Backbone */
 var Forecast = (function() { 'use strict';
 
-  var Location = require('./location');
-
   return Backbone.Model.extend({
 
     defaults: {
@@ -12,26 +10,25 @@ var Forecast = (function() { 'use strict';
     },
 
     url: function() {
+      var location = this.get('location');
       // DEV
       // API from OpenWeatherMap :)
-      return 'http://api.openweathermap.org/data/2.5/weather?q=Toronto&units=metric&APPID=b61a972176062e8cb65572109884d904';
+      return 'http://api.openweathermap.org/data/2.5/weather?lat='+location.get('lat')+'&lon='+location.get('lon')+'&units=metric&APPID=b61a972176062e8cb65572109884d904';
       // return '/data/data-toronto.json';
     },
 
     parse: function(response, options) {
       // Adding the rounded temperature to the model data
       response.temperature =  Math.round(response.main.temp);
+
       // Returning the parse data
       return response;
     },
 
     initialize: function() {
       var self = this;
-      // Getting the user's location (currently disabled)
-      var location = new Location();
       // Fetching data from the API
       self.fetch({
-        dataType: 'jsonp',
         success: function() {
           // Calculating the apparel suggestion
           self.calcSweather();
