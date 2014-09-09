d8af7c23d0de8c14f8e1ec565e05a6fbd77af380
ItsJonQ <itsjonq@gmail.com>
2014-09-08 21:20:02 -0400

    Updated location to fetch user's coords

--- app/scripts/models/location.js
+++ app/scripts/models/location.js
@@ -5,7 +5,8 @@ var Location = (function() { 'use strict';
 
     defaults: {
       'latitude': null,
-      'longitude': null
+      'longitude': null,
+      'status': false
     },
 
     initialize: function() {
@@ -13,13 +14,16 @@ var Location = (function() { 'use strict';
 
       if ('geolocation' in navigator) {
 
-        // navigator.geolocation.getCurrentPosition(function(position) {
-        //   self.set('latitude', position.coords.latitude);
-        //   self.set('longitude', position.coords.longitude);
-        // });
+        navigator.geolocation.getCurrentPosition(function(position) {
+          self.set('lat', position.coords.latitude);
+          self.set('lon', position.coords.longitude);
 
-        this.set('latitude', true);
-        this.set('longitude', true);
+          self.set('status', true);
+
+          return true;
+        });
+        // this.set('latitude', true);
+        // this.set('longitude', true);
 
       } else {
         return false;
